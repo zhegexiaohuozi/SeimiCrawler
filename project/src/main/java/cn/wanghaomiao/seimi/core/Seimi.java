815f37de30f7a8d8015c7d829ff3fef95fb9fdc5
xiaohuo <et.tw@163.com>
2016-04-10 00:20:47 +0800

    prepare to support SeimiAgent

--- project/src/main/java/cn/wanghaomiao/seimi/core/Seimi.java
+++ project/src/main/java/cn/wanghaomiao/seimi/core/Seimi.java
@@ -1,7 +1,7 @@
 package cn.wanghaomiao.seimi.core;
 
 /*
-   Copyright 2015 - now original author
+   Copyright 2015 Wang Haomiao<et.tw@163.com>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- project/src/main/java/cn/wanghaomiao/seimi/utils/StructValidator.java
+++ project/src/main/java/cn/wanghaomiao/seimi/utils/StructValidator.java
@@ -1,6 +1,6 @@
 package cn.wanghaomiao.seimi.utils;
 /*
-   Copyright 2015 - now original author
+   Copyright 2015 Wang Haomiao<et.tw@163.com>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
