2284697e0cde0512bb521fc902289c5b5408e3a1
xiaohuo <et.tw@163.com>
2016-05-31 00:28:59 +0800

    update

--- demo/src/main/java/cn/wanghaomiao/crawlers/UseCookie.java
+++ demo/src/main/java/cn/wanghaomiao/crawlers/UseCookie.java
@@ -6,7 +6,9 @@ import cn.wanghaomiao.seimi.struct.Response;
 import cn.wanghaomiao.seimi.def.BaseSeimiCrawler;
 import cn.wanghaomiao.xpath.model.JXDocument;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 用注解进行配置
@@ -18,30 +20,32 @@ import java.util.List;
 public class UseCookie extends BaseSeimiCrawler {
     @Override
     public String[] startUrls() {
-        //url##post,这种格式的起始URL是告诉seimi处理器，这个URL要以POST请求处理，默认采用GET
-        return new String[]{"http://passport.cnblogs.com/user/signin?ReturnUrl=http%3a%2f%2fi.cnblogs.com%2f##post"};
+        //用于触发第一个回调函数
+        return new String[]{"http://www.douban.com"};
     }
 
     @Override
     public void start(Response response) {
-        JXDocument doc = response.document();
-        try {
-            List<Object> urls = doc.sel("//a[@class='titlelnk']/@href");
-            logger.info("{}", urls.size());
-            for (Object s:urls){
-                push(new Request(s.toString(),"getTitle"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //提交登陆请求
+        Request login = Request.build("https://accounts.douban.com/login","afterLogin");
+
+        Map<String,String> params = new HashMap<>();
+        params.put("source","main");
+        params.put("redir","https://www.douban.com/mine/");
+        params.put("form_email","xx");
+        params.put("form_password","xx");
+        params.put("login","登录");
+        login.setParams(params);
+        push(login);
     }
-    public void getTitle(Response response){
-        JXDocument doc = response.document();
-        try {
-            logger.info("url:{} {}", response.getUrl(), doc.sel("//h1[@class='postTitle']/a/text()|//a[@id='cb_post_title_url']/text()"));
-            //do something
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+    public void afterLogin(Response response){
+        logger.info(response.getContent());
+//        push(Request.build("https://www.douban.com/mine/","minePage"));
     }
+
+//    public void minePage(Response response){
+//        logger.info(response.getContent());
+//    }
+
 }
