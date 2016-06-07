2b5c2264ecc01010dd73f4ca52c4396e25f387fa
xiaohuo <et.tw@163.com>
2016-06-08 01:43:58 +0800

    准备新一版

--- project/src/main/java/cn/wanghaomiao/seimi/core/RequestGenerator.java
+++ project/src/main/java/cn/wanghaomiao/seimi/core/RequestGenerator.java
@@ -1,13 +1,12 @@
 package cn.wanghaomiao.seimi.core;
 
 import cn.wanghaomiao.seimi.def.BaseSeimiCrawler;
-import cn.wanghaomiao.seimi.exception.SeimiInitExcepiton;
 import cn.wanghaomiao.seimi.exception.SeimiProcessExcepiton;
 import cn.wanghaomiao.seimi.http.HttpMethod;
 import cn.wanghaomiao.seimi.struct.CrawlerModel;
 import cn.wanghaomiao.seimi.struct.Request;
 import com.alibaba.fastjson.JSON;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.RequestBuilder;
 
@@ -18,46 +17,48 @@ import java.util.Map;
  * @since 2016/4/14.
  */
 public class RequestGenerator {
-    public static RequestBuilder getHttpRequestBuilder(Request request, CrawlerModel crawlerModel){
+    public static RequestBuilder getHttpRequestBuilder(Request request, CrawlerModel crawlerModel) {
         RequestBuilder requestBuilder;
         BaseSeimiCrawler crawler = crawlerModel.getInstance();
-        if (request.isUseSeimiAgent()){
-            if (StringUtils.isBlank(crawler.seimiAgentHost())){
+        if (request.isUseSeimiAgent()) {
+            if (StringUtils.isBlank(crawler.seimiAgentHost())) {
                 throw new SeimiProcessExcepiton("SeimiAgentHost is blank.");
             }
-            String seimiAgentUrl = "http://"+crawler.seimiAgentHost()+(crawler.seimiAgentPort()!=80?(":"+crawler.seimiAgentPort()):"")+"/doload";
+            String seimiAgentUrl = "http://" + crawler.seimiAgentHost() + (crawler.seimiAgentPort() != 80 ? (":" + crawler.seimiAgentPort()) : "") + "/doload";
             requestBuilder = RequestBuilder.post().setUri(seimiAgentUrl);
-            requestBuilder.addParameter("url",request.getUrl());
-            if (StringUtils.isNotBlank(crawler.proxy())){
-                requestBuilder.addParameter("proxy",crawler.proxy());
+            requestBuilder.addParameter("url", request.getUrl());
+            if (StringUtils.isNotBlank(crawler.proxy())) {
+                requestBuilder.addParameter("proxy", crawler.proxy());
             }
-            if (request.getSeimiAgentRenderTime()>0){
-                requestBuilder.addParameter("renderTime",String.valueOf(request.getSeimiAgentRenderTime()));
+            if (request.getSeimiAgentRenderTime() > 0) {
+                requestBuilder.addParameter("renderTime", String.valueOf(request.getSeimiAgentRenderTime()));
             }
-            if (StringUtils.isNotBlank(request.getSeimiAgentScript())){
-                requestBuilder.addParameter("script",request.getSeimiAgentScript());
+            if (StringUtils.isNotBlank(request.getSeimiAgentScript())) {
+                requestBuilder.addParameter("script", request.getSeimiAgentScript());
             }
             //如果针对SeimiAgent的请求设置是否使用cookie，以针对请求的设置为准，默认使用全局设置
-            if ((request.isSeimiAgentUseCookie()==null&&crawlerModel.isUseCookie())||(request.isSeimiAgentUseCookie()!=null&&request.isSeimiAgentUseCookie())){
-                requestBuilder.addParameter("useCookie","1");
+            if ((request.isSeimiAgentUseCookie() == null && crawlerModel.isUseCookie()) || (request.isSeimiAgentUseCookie() != null && request.isSeimiAgentUseCookie())) {
+                requestBuilder.addParameter("useCookie", "1");
             }
-            if (request.getParams()!=null&&request.getParams().size()>0){
+            if (request.getParams() != null && request.getParams().size() > 0) {
                 requestBuilder.addParameter("postParam", JSON.toJSONString(request.getParams()));
             }
-        }else {
-            if (HttpMethod.POST.equals(request.getHttpMethod())){
+        } else {
+            if (HttpMethod.POST.equals(request.getHttpMethod())) {
                 requestBuilder = RequestBuilder.post().setUri(request.getUrl());
-            }else {
+            } else {
                 requestBuilder = RequestBuilder.get().setUri(request.getUrl());
             }
             RequestConfig config = RequestConfig.custom().setProxy(crawlerModel.getProxy()).setCircularRedirectsAllowed(true).build();
             config.isRelativeRedirectsAllowed();
-            if (request.getParams()!=null){
-                for (Map.Entry<String,String> entry:request.getParams().entrySet()){
-                    requestBuilder.addParameter(entry.getKey(),entry.getValue());
+            if (request.getParams() != null) {
+                for (Map.Entry<String, String> entry : request.getParams().entrySet()) {
+                    requestBuilder.addParameter(entry.getKey(), entry.getValue());
                 }
             }
-            requestBuilder.setConfig(config).setHeader("User-Agent",crawler.getUserAgent());
+            requestBuilder.setConfig(config).setHeader("User-Agent", crawlerModel.isUseCookie()?crawlerModel.getCurrentUA():crawler.getUserAgent());
+            requestBuilder.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            requestBuilder.setHeader("Accept-Language","zh-CN,zh;q=0.8,en;q=0.6");
         }
         return requestBuilder;
     }
--- project/src/main/java/cn/wanghaomiao/seimi/def/BaseSeimiCrawler.java
+++ project/src/main/java/cn/wanghaomiao/seimi/def/BaseSeimiCrawler.java
@@ -18,6 +18,7 @@ package cn.wanghaomiao.seimi.def;
 import cn.wanghaomiao.seimi.core.SeimiCrawler;
 import cn.wanghaomiao.seimi.core.SeimiQueue;
 import cn.wanghaomiao.seimi.struct.Request;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.http.client.CookieStore;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.slf4j.Logger;
@@ -33,7 +34,26 @@ public abstract class BaseSeimiCrawler implements SeimiCrawler {
     protected CookieStore cookieStore = new BasicCookieStore();
     protected Logger logger = LoggerFactory.getLogger(getClass());
     protected String crawlerName;
-
+    protected String[] defUAs = new String[]{
+            "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+            "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)",
+            "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)",
+            "Mozilla/5.0 (Windows; U; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)",
+            "Mozilla/6.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1",
+            "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:15.0) Gecko/20120910144328 Firefox/15.0.2",
+            "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201",
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9a3pre) Gecko/20070330",
+            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13; ) Gecko/20101203",
+            "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+            "Opera/9.80 (X11; Linux x86_64; U; fr) Presto/2.9.168 Version/11.50",
+            "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; de) Presto/2.9.168 Version/11.52",
+            "Mozilla/5.0 (Windows; U; Win 9x 4.90; SG; rv:1.9.2.4) Gecko/20101104 Netscape/9.1.0285",
+            "Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.7pre) Gecko/20070815 Firefox/2.0.0.6 Navigator/9.0b3",
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6"
+    };
     protected void push(Request request) {
         request.setCrawlerName(crawlerName);
         queue.push(request);
@@ -41,7 +61,8 @@ public abstract class BaseSeimiCrawler implements SeimiCrawler {
 
     @Override
     public String getUserAgent() {
-        return "SeimiCrawler/JsoupXpath";
+        int index = RandomUtils.nextInt(0,defUAs.length);
+        return defUAs[index];
     }
 
     @Override
