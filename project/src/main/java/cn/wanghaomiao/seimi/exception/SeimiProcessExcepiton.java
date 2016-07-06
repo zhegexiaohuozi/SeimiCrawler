b840acebbdf54f15e2f46f00ea7c7eed6cdcf486
xiaohuo <et.tw@163.com>
2016-07-06 21:23:26 +0800

    v1.1.0

--- project/src/main/java/cn/wanghaomiao/seimi/exception/SeimiProcessExcepiton.java
+++ project/src/main/java/cn/wanghaomiao/seimi/exception/SeimiProcessExcepiton.java
@@ -1,4 +1,3 @@
-package cn.wanghaomiao.seimi.exception;
 /*
    Copyright 2015 Wang Haomiao<et.tw@163.com>
 
@@ -14,6 +13,7 @@ package cn.wanghaomiao.seimi.exception;
    See the License for the specific language governing permissions and
    limitations under the License.
  */
+package cn.wanghaomiao.seimi.exception;
 
 
 /**
